ec4e5a570d35a165006a75a7cc63eac0677df8f4
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-02-07 07:13:44 -0600

    Updates

--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -1,33 +1,27 @@
 import { useMoralis } from "react-moralis"
 import { useEffect } from "react"
 
-import { Box, IconButton, Text } from "@chakra-ui/react"
+import { Box, IconButton, Text, MenuButton, MenuList, MenuItem, Menu } from "@chakra-ui/react"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faWallet } from "@fortawesome/free-solid-svg-icons"
 
 export default function ManualHeader() {
-    // useMoralis is a React "hook" that hooks into the React state and lifecycle features
     const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading } =
         useMoralis()
 
+    // Check if the user has already connected to a wallet
     useEffect(() => {
         if (isWeb3Enabled) return
-        if (typeof window !== "undefined") {
-            if (window.localStorage.getItem("connected")) {
-                enableWeb3()
-            }
+        if (window!.localStorage.getItem("connected")) {
+            enableWeb3()
         }
     }, [isWeb3Enabled])
-    // No dependency array:
-    //   run anytime something re-renders
-    //   CAREFUL with this!! Because then you can get circular render
-    // Blank dependency array:
-    //   run once on load / rerender
-    //   dependencies in the array, run anytime something in there changes
 
+    // When the MetaMask account changes, update the state
     useEffect(() => {
         Moralis.onAccountChanged((account) => {
+            // if (!isWeb3Enabled) return
             console.log(`Account changed to ${account}`)
             if (account == null) {
                 window.localStorage.removeItem("connected")
@@ -41,20 +35,30 @@ export default function ManualHeader() {
         <Box>
             {account ? (
                 <Box paddingLeft={10}>
-                    <IconButton aria-label={"Wallet connected"}>
-                        <Text margin={3}>
-                            {account.slice(0, 4)}...{account.slice(account.length - 4)}
-                        </Text>
-                    </IconButton>
+                    <Menu>
+                        <MenuButton as={IconButton}>
+                            <Text margin={3}>
+                                {account.slice(0, 4)}...{account.slice(account.length - 4)}
+                            </Text>
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem
+                                onClick={async () => {
+                                    window.localStorage.removeItem("connected")
+                                    await deactivateWeb3()
+                                }}
+                            >
+                                Disconnect
+                            </MenuItem>
+                        </MenuList>
+                    </Menu>
                 </Box>
             ) : (
                 <Box paddingLeft={10}>
                     <IconButton
                         onClick={async () => {
                             await enableWeb3()
-                            if (typeof window !== "undefined") {
-                                window.localStorage.setItem("connected", "injected")
-                            }
+                            window!.localStorage.setItem("connected", "injected")
                         }}
                         disabled={isWeb3EnableLoading}
                         aria-label={"Connect wallet"}
